bb929c121452948773f239f0789add7c9503cb6a
luciosantibañez <luciosantibanez99@gmail.com>
2024-11-07 12:06:14 -0300

    creando ajax con productos en json

--- scripts.js
+++ scripts.js
@@ -11,6 +11,13 @@ const username = document.getElementById("username");
 const password = document.getElementById("password");
 const button = document.getElementById("button");
 
-button.addEventListener("click", (e) => {
-  e.preventDefault();
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/componentes/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer").innerHTML = data;
+    })
+    .catch((error) => console.error("Error al cargar el footer:", error));
 });
